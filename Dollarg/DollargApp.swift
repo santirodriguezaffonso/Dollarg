c59df0d7739940238fd996fcd2bf81126170e233
Santiago Rodriguez Affonso <rodriguezsant@icloud.com>
2022-05-01 19:51:18 -0300

    Initial Commit

---
+++ Dollarg/DollargApp.swift
@@ -0,0 +1,17 @@
+//
+//  DollargApp.swift
+//  Dollarg
+//
+//  Created by Santiago Rodriguez Affonso on 01/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct DollargApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
